81868a6caef8e8ce2f9bf33922c75f5487dc9416
Sean Murphy <sean@iamseanmurphy.com>
2025-04-23 13:22:18 +0000

    3.3.4

--- wp-menu-icons/vendor/composer/installed.php
+++ wp-menu-icons/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'quadlayers/wp-menu-icons',
-        'pretty_version' => 'v3.3.3',
-        'version' => '3.3.3.0',
-        'reference' => '748d4d47a4de8d3c3cddaaa9a38b9d9ed9202df8',
+        'pretty_version' => 'v3.3.4',
+        'version' => '3.3.4.0',
+        'reference' => 'b8f316eabe4ddc018df1051e753ad2c178396b08',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'automattic/jetpack-assets' => array(
-            'pretty_version' => 'v4.0.17',
-            'version' => '4.0.17.0',
-            'reference' => 'eb3177809fb9ada68975ccb2769577e6af0886fc',
+            'pretty_version' => 'v4.0.18',
+            'version' => '4.0.18.0',
+            'reference' => '836395696571edc7c9301ce44806ba945fac731e',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../../jetpack_vendor/automattic/jetpack-assets',
             'aliases' => array(),
@@ -38,9 +38,9 @@
             'dev_requirement' => false,
         ),
         'automattic/jetpack-constants' => array(
-            'pretty_version' => 'v3.0.6',
-            'version' => '3.0.6.0',
-            'reference' => '163424a31d8bf0cafc1344b24bb8c4cdddb7142f',
+            'pretty_version' => 'v3.0.7',
+            'version' => '3.0.7.0',
+            'reference' => 'e27857a70170ba8cd76aa35e9781fbd8799a7d05',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../../jetpack_vendor/automattic/jetpack-constants',
             'aliases' => array(),
@@ -56,9 +56,9 @@
             'dev_requirement' => false,
         ),
         'quadlayers/wp-menu-icons' => array(
-            'pretty_version' => 'v3.3.3',
-            'version' => '3.3.3.0',
-            'reference' => '748d4d47a4de8d3c3cddaaa9a38b9d9ed9202df8',
+            'pretty_version' => 'v3.3.4',
+            'version' => '3.3.4.0',
+            'reference' => 'b8f316eabe4ddc018df1051e753ad2c178396b08',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
